1f0afa74b8c96ac94af587eaa196090ff68acddb
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-01 17:54:23 +0600

    add knowledge css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Knowledge from './components/Knowledge/Knowledge';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Knowledge></Knowledge>
     </div>
   );
 }
